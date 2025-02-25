177c97c995c4b51cd32e8ab37ac35116630f4f99
Skyman-Arias <zap1330@amerike.edu.mx>
2025-02-25 08:55:42 -0600

    Update main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+/*
+University Amerike
+Author: Sebastian Arias
+Practice #2
+Date: 25/02/25
+Description: This programm needs to read the data of the student and using escape sequences
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    cout <<"Complete name: Sebastian arias\n";
+    cout <<"Age: 21\n";
+    cout <<"Ocupation: Student university\n";
+    cout <<"city of origin: Guadalajara\n";
+    cout <<"Favorite Hobbie: Playing videogames\n";
+    cout <<"Favorite food: Pacholas\n";
+    cout <<"Recommended music: Rock\n";
+
+    return 0;
+     
+}
